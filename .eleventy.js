3df8c13c9f1a9d4f7f6b3819ea3c30ff678c1b62
marcus65001 <41275811+marcus65001@users.noreply.github.com>
2023-03-05 14:36:25 -0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -42,6 +42,7 @@ module.exports = function (eleventyConfig) {
 
   // Copy Image Folder to /_site
   eleventyConfig.addPassthroughCopy("./src/static/img");
+  eleventyConfig.addPassthroughCopy("./src/static/file");
 
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
